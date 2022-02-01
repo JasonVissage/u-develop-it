493958c7e13ec9ebeef5ecc6b24fa6bb1155c9b2
Jason Vissage <92236402+JasonVissage@users.noreply.github.com>
2022-02-01 07:19:43 -0600

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: 'Ja$on120180',
+      password: 'root',
       database: 'election'
     },
     console.log('Connected to the election database.') 
